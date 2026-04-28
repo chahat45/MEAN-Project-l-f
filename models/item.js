2120f012e746dc8b6890c654fa99c4b112566797
Chahat <chahat7.2006@gmail.com>
2026-04-28 21:55:31 +0530

    user

--- models/item.js
+++ models/item.js
@@ -1,12 +1,35 @@
 const mongoose = require("mongoose");
 
 const ItemSchema = new mongoose.Schema({
+
   name: String,
   description: String,
   location: String,
-  date: String,
-  contact: String,
-  status: String
-});
+
+  status: {
+    type: String,
+    default: "Lost"
+  },
+
+  reward: {
+    type: Number,
+    default: 0
+  },
+
+  image: String, // store image URL or base64
+
+  gmapLink: String, // optional
+
+  createdBy: String, // rollNo of user
+
+  foundBy: String,
+
+  collectionVenue: String,
+  qrCode: String, // image or text
+
+  confirmByOwner: { type: Boolean, default: false },
+  confirmByFinder: { type: Boolean, default: false }
+
+}, { timestamps: true });
 
 module.exports = mongoose.model("Item", ItemSchema);
--- routes/items.js
+++ routes/items.js
@@ -4,22 +4,22 @@ const Item = require("../models/item");
 
 router.post("/add", async (req, res) => {
 
-  const item = new Item(req.body);
+  console.log(req.body); // 👈 ADD THIS
 
-  try {
-    await item.save();
-    res.json(item);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+  const newItem = new Item(req.body);
+  await newItem.save();
+
+  res.send("Item added");
 
 });
 
 router.get("/all", async (req, res) => {
 
   try {
-    const items = await Item.find();
-    res.json(items);
+     const items = await Item.find()
+    .sort({ reward: -1, createdAt: -1 });
+
+  res.json(items);
   } catch (error) {
     res.status(500).send(error);
   }
@@ -53,6 +53,30 @@ router.delete("/delete/:id", async (req, res) => {
     res.status(500).send(error);
   }
 
+});
+
+//new
+router.put("/confirm/:id", async (req, res) => {
+
+  const item = await Item.findById(req.params.id);
+
+  if(req.body.user === item.createdBy){
+    item.confirmByOwner = true;
+  }
+
+  if(req.body.user === item.foundBy){
+    item.confirmByFinder = true;
+  }
+
+  // delete only when both confirmed
+  if(item.confirmByOwner && item.confirmByFinder){
+    await Item.findByIdAndDelete(req.params.id);
+    return res.send("Item completed and deleted");
+  }
+
+  await item.save();
+
+  res.send("Confirmation saved");
 });
 
 module.exports = router;
